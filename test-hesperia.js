d8dfc8c330a1b9040e967444694202f668bb389e
Julián Muñoz Villanueva <101838070+munozjuliann@users.noreply.github.com>
2026-09-14 23:17:01 -0300

    Update test-hesperia.js

--- test-hesperia.js
+++ test-hesperia.js
@@ -1,1 +1,33 @@
-alert("HESPERIA ARCHIVO NUEVO");
+(function () {
+
+  function buscarProducto() {
+
+    var producto = document.querySelector(".product-vip");
+
+    if (producto) {
+
+      var bloque = document.createElement("div");
+
+      bloque.innerHTML = "HESPERIA ENCONTRÓ EL PRODUCTO";
+
+      bloque.style.cssText =
+        "margin:40px 0!important;" +
+        "padding:40px!important;" +
+        "background:#000!important;" +
+        "color:#fff!important;" +
+        "font-size:24px!important;" +
+        "text-align:center!important;";
+
+      producto.appendChild(bloque);
+
+      console.log("HESPERIA: producto encontrado");
+
+      return;
+    }
+
+    setTimeout(buscarProducto, 500);
+  }
+
+  buscarProducto();
+
+})();
